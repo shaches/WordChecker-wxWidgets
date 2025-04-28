ee29900da63acbb3e730cddd5ac8d8a8659ac99a
shaches <shaches7@proton.me>
2025-04-28 23:29:50 +0300

    0.0.1

--- main.cpp
+++ main.cpp
@@ -1,81 +1,245 @@
-// wxWidgets "Hello World" Program
- 
-// For compilers that support precompilation, includes "wx/wx.h".
-#include <wx/wxprec.h>
- 
-#ifndef WX_PRECOMP
-    #include <wx/wx.h>
-#endif
- 
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <wx/wx.h>
+#include <wx/richtext/richtextctrl.h>
+#include <wx/richtext/richtextstyles.h>
+#include <wx/richtext/richtextxml.h>
+#include <wx/sizer.h>
+#include <wx/button.h> 
+
 class MyApp : public wxApp
 {
 public:
     virtual bool OnInit();
 };
- 
+
 class MyFrame : public wxFrame
 {
 public:
-    MyFrame();
- 
+    MyFrame(const wxString& title);
+
 private:
-    void OnHello(wxCommandEvent& event);
-    void OnExit(wxCommandEvent& event);
-    void OnAbout(wxCommandEvent& event);
+    wxRichTextCtrl* m_richTextCtrlLeft;
+    wxRichTextCtrl* m_richTextCtrlRight;
+
+    wxButton* m_button1;
+    wxButton* m_button2;
+    wxButton* m_button3;
+
+    void OnButton1Click(wxCommandEvent& event);
+    void OnButton2Click(wxCommandEvent& event);
+	void OnButton3Click(wxCommandEvent& event);
+
 };
- 
-enum
-{
-    ID_Hello = 1
-};
- 
+
 wxIMPLEMENT_APP(MyApp);
- 
+
 bool MyApp::OnInit()
 {
-    MyFrame *frame = new MyFrame();
+    wxInitAllImageHandlers();
+    wxRichTextBuffer::AddHandler(new wxRichTextXMLHandler);
+
+    MyFrame *frame = new MyFrame("WordChecker");
     frame->Show(true);
     return true;
 }
- 
-MyFrame::MyFrame()
-    : wxFrame(NULL, wxID_ANY, "Hello World")
+
+MyFrame::MyFrame(const wxString& title)
+    : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(900, 600))
 {
-    wxMenu *menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-                     "Help string shown in status bar for this menu item");
-    menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT);
- 
-    wxMenu *menuHelp = new wxMenu;
-    menuHelp->Append(wxID_ABOUT);
- 
-    wxMenuBar *menuBar = new wxMenuBar;
-    menuBar->Append(menuFile, "&File");
-    menuBar->Append(menuHelp, "&Help");
- 
-    SetMenuBar( menuBar );
- 
-    CreateStatusBar();
-    SetStatusText("Welcome to wxWidgets!");
- 
-    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
-    Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
-    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+    wxPanel* mainPanel = new wxPanel(this, wxID_ANY);
+
+    m_richTextCtrlLeft = new wxRichTextCtrl(mainPanel, wxID_ANY, wxEmptyString,
+                                            wxDefaultPosition, wxDefaultSize,
+                                            wxVSCROLL | wxHSCROLL | wxBORDER_SUNKEN);
+
+    m_richTextCtrlRight = new wxRichTextCtrl(mainPanel, wxID_ANY, wxEmptyString,
+                                             wxDefaultPosition, wxDefaultSize,
+                                             wxVSCROLL | wxHSCROLL | wxBORDER_SUNKEN | wxTE_READONLY);
+
+    int button1Id = wxID_HIGHEST + 1;
+    int button2Id = wxID_HIGHEST + 2;
+	int button3Id = wxID_HIGHEST + 3;
+
+    m_button1 = new wxButton(mainPanel, wxID_HIGHEST + 1, "Kontrol Et");
+    m_button2 = new wxButton(mainPanel, wxID_HIGHEST + 2, "Kaydet");
+	m_button3 = new wxButton(mainPanel, wxID_HIGHEST + 3, "Ayarlar");
+
+	// --- Sizers ---
+    wxBoxSizer *buttonSizer = new wxBoxSizer(wxVERTICAL);
+    buttonSizer->Add(
+        m_button1,
+        0,                	// Proportion 0: Button doesn't grow vertically
+        wxEXPAND | wxBOTTOM,// Flag: Expand horizontally to fill column, add space below
+        5                 	// Border size
+    );
+    buttonSizer->Add(
+        m_button2,
+        0,                	// Proportion 0: Button doesn't grow vertically
+        wxEXPAND | wxBOTTOM,// Flag: Expand horizontally to fill column
+        5                 	// Border size (no border below the last button)
+    );
+    buttonSizer->Add(         // <-- ADD THIS BLOCK
+    	m_button3,
+    	0,                    // Proportion 0
+    	wxEXPAND,             // Flag: Expand horizontally
+    	0                     // Border size (0, since it's the last one for now)
+    );
+
+    wxBoxSizer *mainSizer = new wxBoxSizer(wxHORIZONTAL);
+
+    mainSizer->Add(
+        m_richTextCtrlLeft,
+        1,                      // Proportion 1: Takes half of the resizable space
+        wxEXPAND | wxALL,       // Flags: Expand fully, add border on all sides
+        5                       // Border size
+    );
+
+    mainSizer->Add(
+        m_richTextCtrlRight,
+        1,                      // Proportion 1: Takes the other half of resizable space
+        wxEXPAND | wxTOP | wxBOTTOM | wxLEFT, // Flags: Expand, add border on 3 sides
+        5                       // Border size (leave right side open for buttons)
+    );
+
+    mainSizer->Add(
+        buttonSizer,
+        0,                      // Proportion 0: Button column takes minimum width, doesn't resize horizontally
+        wxEXPAND | wxTOP | wxBOTTOM | wxRIGHT, // Flags: Allow vertical expansion within its cell, add border on 3 sides
+        5                       // Border size
+    );
+	
+    mainPanel->SetSizer(mainSizer);
+
+    // Create a sizer for the frame itself to hold the mainPanel
+    // This ensures the panel resizes correctly with the frame
+    wxBoxSizer* frameSizer = new wxBoxSizer(wxVERTICAL);
+    frameSizer->Add(mainPanel, 1, wxEXPAND | wxALL, 0); // Panel expands to fill frame
+    this->SetSizerAndFit(frameSizer); // Set sizer for the frame
+
+    this->SetMinSize(wxSize(500, 300));
+
+	// --- Sizers ---
+    m_richTextCtrlRight->BeginBold();
+    m_richTextCtrlRight->WriteText("Kontrol Et tusuna bas!"); // Turkish character support required
+    m_richTextCtrlRight->EndBold();
+
+    mainPanel->Bind(wxEVT_BUTTON, &MyFrame::OnButton1Click, this, button1Id);
+    mainPanel->Bind(wxEVT_BUTTON, &MyFrame::OnButton2Click, this, button2Id);
+    mainPanel->Bind(wxEVT_BUTTON, &MyFrame::OnButton3Click, this, button3Id);
+
 }
- 
-void MyFrame::OnExit(wxCommandEvent& event)
-{
-    Close(true);
-}
- 
-void MyFrame::OnAbout(wxCommandEvent& event)
-{
-    wxMessageBox("This is a wxWidgets Hello World example",
-                 "About Hello World", wxOK | wxICON_INFORMATION);
-}
- 
-void MyFrame::OnHello(wxCommandEvent& event)
-{
-    wxLogMessage("Hello world from wxWidgets!");
-}
+
+   void MyFrame::OnButton1Click(wxCommandEvent& event)
+   {
+		wxString textFromLeft = m_richTextCtrlLeft->GetValue();
+
+		// vvv Check Happens Here vvv
+
+		std::string text = textFromLeft.ToStdString();
+
+		std::replace(text.begin(), text.end(), '\n', ' ');
+		std::replace(text.begin(), text.end(), '\t', ' ');
+
+		text += " ";
+
+		std::vector<std::array<std::string,2>> index;
+
+		// std::vector<std::pair<std::string, int>> index;
+
+		// Seperate the sentence into words
+		bool did_it = false;
+		int how_much_did = 0;
+		std::array<std::string, 2> word;
+		for(int i = 0; i < text.size(); i++)
+		{
+			if(did_it && text.at(i) == ' '){
+				for(int j = 0; j < i; j++){
+					if(text.at(j) != ' '){
+						word.at(0) += text.at(j);	
+					}
+				}
+				index.push_back(word);
+				word = {"",""};
+				text.erase(0,i);
+				i = 0;
+			}
+
+			if(text.at(i) == ' '){
+				did_it = false;
+			}
+			else{
+				did_it = true;
+				how_much_did++;
+			}
+		}
+
+		if(how_much_did == 0){
+			wxLogMessage("Lutfen soldaki kutuya bir seyler yaz!");
+			return;
+		}
+
+		text = "";
+
+		if(index.size() == 1){
+			text += index.at(0).at(0) + " : 1\n";
+			textFromLeft = text;
+			m_richTextCtrlRight->SetValue(textFromLeft);
+			return;
+		}
+
+		for(int i = 0; i < index.size()-1; i++){
+			for(int j = 0; j < index.size()-1-i; j++){
+				if(index.at(j).at(0) > index.at(j+1).at(0)){
+					std::swap(index.at(j), index.at(j+1));
+				}
+			}
+		}
+
+		int count{0};
+		for(int i = 0; i < index.size()-1; i++){
+			if(index.at(i).at(0) == index.at(i+1).at(0)){
+				index.erase(index.begin() + i);
+				i -= 1;
+				count++;
+			}
+			else{
+				index.at(i).at(1) = std::to_string(count+1);
+				count = 0;
+			}
+
+			if(i == index.size()-2){
+				index.at(i+1).at(1) = std::to_string(count+1);
+			}
+		}
+
+		for(int i = 0; i < index.size()-1; i++){
+			for(int j = 0; j < index.size()-1-i; j++){
+				if(std::stoi(index.at(j).at(1)) < std::stoi(index.at(j+1).at(1))){
+					std::swap(index.at(j), index.at(j+1));
+				}
+			}
+		}
+
+		for(int i = 0; i < index.size(); i++){
+			text += index.at(i).at(0) + " : " + index.at(i).at(1) + '\n';
+		}
+
+		textFromLeft = text;
+
+		// ^^^ Check Happens Here ^^^
+
+		m_richTextCtrlRight->SetValue(textFromLeft);
+   }
+
+   void MyFrame::OnButton2Click(wxCommandEvent& event)
+   {
+       wxLogMessage("Yakinda Geliyor!");
+   }
+
+   void MyFrame::OnButton3Click(wxCommandEvent& event)
+   {
+       wxLogMessage("Yakinda Geliyor!");
+   }
